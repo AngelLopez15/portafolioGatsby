9c0fe919e36ffc387f5ef88eb69e98b1ae9223e3
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-10-26 22:50:21 -0600

    Componente del perfil terminado

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,11 +1,56 @@
 import React from 'react'
+import { Col, Container, Image, Row } from 'react-bootstrap'
+import imageProfile from "../../images/avatar.png"
 import "./Profile.scss"
 
 export default function Profile() {
+  
+  // Ejemplo de como pintar la informacion por si 
+  // nos esta llegando desde una peticion
+  const data = [
+    {
+      title: "Edad",
+      info:"29"
+    },
+    {
+      title:"Dirección",
+      info:"0107 Hackett Throughway"
+    },
+    {
+      title:"Email",
+      info:"Roxane_Daugherty70@hotmail.com"
+    }
+  ]
+  
   return (
     <div className="profile">
       <div className="profile__wallpaper" />
       <div className="profile__dark"/>
+      <Container className="profile__box">
+        <Row className="profile__info">
+          <Col xs={12} md={4}>
+            <Image src={imageProfile} alt="avatar" fluid />
+          </Col>
+          <Col xs={12} md={8} className="profile__data">
+            <span className="profile__data-greeting">¡Hola!</span>
+            <p className="profile__data-name">Angel Octavio López Cruz</p>
+            <p>Frontend Developer</p>
+            <hr />
+            <div className="profile__data-info">
+              <div className="profile__data-item">
+                <p className="profile__data-item-phone">Telefono:</p>
+                <p className="profile__data-item-phone-number">55-55-55-55-55</p>
+              </div>
+              {data.map((item, index)=>(
+                <div className="profile__data-item">
+                  <p className="profile__data-item-phone">{item.title}:</p>
+                  <p className="profile__data-item-phone-number">{item.info}</p>
+                </div>
+              ))}
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
