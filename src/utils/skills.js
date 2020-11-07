9cafe3b14df9ec4229a35abb311b9cfda6878dfa
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-11-06 22:06:07 -0600

    Componente skill agregado

---
+++ src/utils/skills.js
@@ -0,0 +1,49 @@
+// En este archivo vamos a tener los arreglos y objetos que necesitamos para 
+// La configuracion de las barras
+export const frontendSkills = [
+  {type: "HTML", level: 95},
+  {type: "CSS 3", level: 90},
+  {type: "SASS", level: 85},
+  {type: "Javascript", level: 80},
+  {type: "React JS", level: 70},
+  {type: "Gatsby", level: 70},
+  {type: "NEXT JS", level: 70},
+  {type: "Vue 3", level: 60},
+]
+
+// Para configurar los colores de las barras
+export const frontendSkillsColors = {
+  bar:'#3498db',
+  title:{
+    text:'#fff',
+    background:"#2980b9",
+  }
+}
+
+export const backendSkills = [
+  {type: "Node", level: 70},
+  {type: "Java", level: 65},
+  {type: "C#", level: 55},
+  {type: "Python", level: 60},
+]
+
+export const backendSkillsColors = {
+  bar:'#00bd3f',
+  title:{
+    text:'#fff',
+    background:"#009331",
+  },
+}
+
+export const desingWeb = [
+  {type:"Adobe XD", level:80},
+  {type:"Figma", level:95},
+]
+
+export const desingWebColors = {
+  bar:"#e51575",
+  title:{
+    text:"#fff",
+    background:"#e51598",
+  },
+}
