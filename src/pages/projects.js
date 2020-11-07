35cd85efa1e5a0d3c83e3f12d08e1773d57829e7
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-11-06 22:30:45 -0600

    Componente projects iniciado

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import { Card, Col, Container, Row } from 'react-bootstrap'
 import BasicLayout from '../layout/BasicLayout'
-import "./index.scss"
+import "./project.scss"
 
 export default function projects() {
   return (
     <BasicLayout
       menuColor="#c4c4c4"
     >
-      <h1>Estamos en Skills</h1>
+    <Container className="projects">
+      <h1 className="projects__title">Proyectos</h1>
+      
+      <Row>
+        <Col xs={12} sm={4} className="projects__project">
+          <Card>
+            <div className="projects__project-image">
+
+            </div>
+            <Card.Body>
+              <Card.Title>Google</Card.Title>
+              <Card.Text>Aut praesentium modi optio nam rem quidem cum maxime. Eius eaque suscipit veniam porro libero sit eum qui. Voluptas aut aliquam id ea quae assumenda adipisci. Quibusdam minima qui architecto modi qui incidunt. Quibusdam eligendi voluptas sed.</Card.Text>
+              <a class="btn" href="" target="_blank" rel="noopener noreferrer">Ver proyectos</a>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container> 
     </BasicLayout>
   )
 }
